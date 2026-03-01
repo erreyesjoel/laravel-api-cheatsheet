7314659f24eccc54ce5a17757f826c0845441956
Joel Erreyes <erreyesjoel.gms@gmail.com>
2026-03-01 22:54:51 +0100

    tasks service ts

--- frontend/src/pages/Tasks/Tasks.tsx
+++ frontend/src/pages/Tasks/Tasks.tsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+import { tasksService } from "../../api/services/tasks.service";
+
 const Tasks = () => {
+    useEffect(() => {
+        const tasksFetch = async () => {
+            try {
+                const tasks = await tasksService.getTasks();
+                console.log("Tareas usuario", tasks);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        tasksFetch();
+    }, []);
+
     return (
         <h1>Tareas usuario</h1>
     );
